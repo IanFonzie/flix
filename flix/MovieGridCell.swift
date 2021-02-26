44ae6d3439fb3a13f4d6a32e81eb32d39dbdbf9d
IanFonzie <fberg_i@hotmail.com>
2021-02-26 13:43:20 -0800

    Add movie detail view, collection view and tab bar navigation.

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Ian Fonberg on 2021-02-26.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
